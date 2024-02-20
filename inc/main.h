bde60adf1d91ff6692a8194d120e7e7b17ce3efd
Adam Slaymark <adam.slaymark@googlemail.com>
2024-02-20 21:13:30 +0000

    ascii artwork

--- inc/main.h
+++ inc/main.h
@@ -5,8 +5,15 @@
 #ifndef HEARING_TEST_BUTTON_MAIN_H
 #define HEARING_TEST_BUTTON_MAIN_H
 
-#include <stdint.h>
+#include <stdio.h>
 
+const char welcomeLogo[1000] =
+
+    " _  ______  ____  _       ____        _   _\n"
+    "| |/ /  _ \\|  _ \\| |     | __ ) _   _| |_| |_ ___  _ __\n"
+    "| ' /| |_) | |_) | |     |  _ \\| | | | __| __/ _ \\| '_ \\ \n"
+    "| . \\|  __/|  _ <| |___  | |_) | |_| | |_| || (_) | | | |\n"
+    "|_|\\_\\_|   |_| \\_\\_____| |____/ \\__,_|\\__|\\__\\___/|_| |_|\n";
 
 
 
