bb79bc1c2b7456f00cf14cd5ae947f6c049b1731
Adam Slaymark <adam.slaymark@googlemail.com>
2024-02-20 22:11:21 +0000

    ascii artwork

---
+++ inc/ANSI_colors.h
@@ -0,0 +1,18 @@
+//
+// Created by adam slaymark on 20/02/2024.
+//
+
+#ifndef HEARING_TEST_BUTTON_ANSI_COLORS_H
+#define HEARING_TEST_BUTTON_ANSI_COLORS_H
+
+// ANSI color codes
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_ORANGE  "\x1b[38;5;208m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+#endif //HEARING_TEST_BUTTON_ANSI_COLORS_H
