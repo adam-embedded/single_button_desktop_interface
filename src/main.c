3ffde41c9200049b9007a4c8771f7174a3b3391b
Adam Slaymark <adam.slaymark@googlemail.com>
2024-02-20 18:42:13 +0000

    broke down core parts in to separate abstractions

--- src/main.c
+++ src/main.c
@@ -3,27 +3,18 @@
 #include <string.h>
 #include <signal.h>
 #include <ctype.h>
-
-#ifdef _WIN32
-#include <winsock2.h>
-#include <ws2tcpip.h>
-#pragma comment(lib, "ws2_32.lib")
-#else
 #include <unistd.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#endif
+
+
 
 /* Private Includes*/
 #include "main.h"
 #include "serial.h"
 #include "device.h"
+#include "socket.h"
 
-/* Variables */
+/* Volatile Variables */
 volatile sig_atomic_t stop = 0;
-static char serialName[256];
-static int32_t baud = 0;
 
 /* Prototypes */
 static void handle_sig(int sig)
@@ -35,6 +26,8 @@ static void handle_sig(int sig)
 
 int main(int argc, char** argv) {
     int16_t PORT = 0;
+    char serialName[256];
+    int32_t baud = 0;
 
     // Parse options
     int index;
@@ -104,91 +97,48 @@ int main(int argc, char** argv) {
     printf("Button Interface Started\n");
     printf("Please wait for device detection");
 
-    ///////
-    /* Serial Setup */
-    ///////
-
+///////
+/* Serial Setup */
+///////
     //Initialise Serial - If there is an issue the library will self terminate the program
     serialInit(serialName, baud);
+///////
+/* End Serial Setup */
+/////
 
-    //Check for device online
-    ReadyStatus();
+///////
+/*Device Setup*/
+///////
+    //Check for device online and initialise device
+    initialiseDevice();
+//////
+/*End Device Setup*/
+//////
 
-
-    //////
-    /* UDP Setup */
-    /////
-
-    // Set up socket
-    int client_socket;
-    struct sockaddr_in server_addr;
-    const char *server_ip = "127.0.0.1";
-    //const int PORT = 8080;
-    const char payload = 6; // Single byte payload
-
-#ifdef _WIN32
-    WSADATA wsa;
-    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
-        printf("WSAStartup failed\n");
-        return 1;
-    }
-#endif
-
-    // Create socket
-    if ((client_socket = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
-        perror("socket failed");
-#ifdef _WIN32
-        WSACleanup();
-#endif
-        exit(EXIT_FAILURE);
-    }
-
-    // Prepare the sockaddr_in structure
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(PORT);
-
-    // Convert IPv4 and IPv6 addresses from text to binary form
-    if (inet_pton(AF_INET, server_ip, &server_addr.sin_addr) <= 0) {
-        perror("Invalid address/ Address not supported");
-#ifdef _WIN32
-        WSACleanup();
-#endif
-        exit(EXIT_FAILURE);
-    }
+//////
+/* UDP Setup */
+//////
+    char server_ip[] = "127.0.0.1";
+    initialiseSocket(&server_ip[0], PORT);
+///////
+/* End socket setup */
+///////
 
 
-
-
-    // Loop to send packets
+    // Loop
     while (!stop) {
-        // Send single datagram packet with 1 byte payload
-        ssize_t send_len = sendto(client_socket, &payload, sizeof(payload), 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
-        if (send_len < 0) {
-            perror("sendto failed");
-#ifdef _WIN32
-            WSACleanup();
-#endif
-            exit(EXIT_FAILURE);
-        }
 
-        printf("Sent a single datagram packet with a 1 byte payload\n");
-
-#ifdef _WIN32
-        Sleep(1000); // Sleep for 1 second
-#else
-        sleep(1); // Sleep for 1 second
-#endif
     }
+
+
 
     printf("* Stopping\n");
 
     // Close socket
-    close(client_socket);
+    closeSocket();
     closeSerial();
 
-#ifdef _WIN32
-    WSACleanup();
-#endif
+
     printf("* Exiting\n");
 
     return 0;
